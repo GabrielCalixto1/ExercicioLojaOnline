e655e787763b689ecdd9ff5f3060e6de6bf239ed
Gabriel Calixto <gabrielcalixtosoares@gmail.com>
2022-04-03 19:59:58 -0300

    Finalizando mudança de local de compra

--- LojaOnline/Carrinho.cs
+++ LojaOnline/Carrinho.cs
@@ -26,7 +26,73 @@ namespace exercicioLojaOnline.LojaOnline
             }
             return total;
         }
-    
+        public void ClienteCompras()
+        {
+            Computador dell = new Computador();
+            VideoGame ps4 = new VideoGame();
+            Celular samsung = new Celular();
+
+            bool opcao3 = true;
+            do
+            {
+                Console.WriteLine($"1- Computador {dell.Marca} \n2- Video Game {ps4.Marca} \n3- Celular {samsung.Marca} ");
+
+                var opcao = Console.ReadLine();
+                switch (opcao)
+                {
+                    case "1":
+                        AdicionarNovoProduto(dell);
+                        break;
+                    case "2":
+                        Console.WriteLine("Deseja adicionar jogos? S ou N");
+                        var jogosOpcao = Console.ReadLine();
+                        if (jogosOpcao == "S")
+                        {
+                            Console.WriteLine("Quantos?");
+                            double quantJogos = double.Parse(Console.ReadLine());
+
+                            ps4.AdicionarJogos(quantJogos);
+                            AdicionarNovoProduto(ps4);
+                        }
+                        else if (jogosOpcao == "N")
+                        {
+                            AdicionarNovoProduto(ps4);
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Opção Inválida");
+                        }
+
+                        break;
+                    case "3":
+                        AdicionarNovoProduto(samsung);
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida.");
+                        break;
+                }
+                Console.WriteLine("Deseja adicionar algo a mais? S ou N");
+                var opcao2 = Console.ReadLine();
+                if (opcao2 == "N")
+                {
+                    opcao3 = false;
+                }
+
+                else if (opcao2 == "S")
+                {
+                    opcao3 = true;
+                }
+                else
+                {
+                    Console.WriteLine("Opção inválida");
+                }
+                ps4.ResetarValor();
+            } while (opcao3 == true);
+
+           
+
+        }
 
     }
 }
